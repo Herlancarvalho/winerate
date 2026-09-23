b0af4a87fcab40304ee1836fb56a89ade6c33946
Herlan Carvalho <herlan.carvalho@hotmail.com>
2026-09-23 20:13:53 -0300

    Cria superusuario no deploy

--- build.sh
+++ build.sh
@@ -4,3 +4,10 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate --noinput
+python manage.py createsuperuser --noinput || true
